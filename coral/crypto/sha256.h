44ee7fe8c319dbf0853b3108dfea3c7fe5c4948c
AdaptiveFlow <adaptiveflow@gmail.com>
2021-11-30 09:10:17 +0900

    Implement TLS (initial)

---
+++ coral/crypto/sha256.h
@@ -0,0 +1,34 @@
+/* Coral library for low level data structure implementation
+  Copyright (C) 1996-2019 AdaptiveFlow
+
+  This library is dual-licensed: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License version 3 as 
+ published by the Free Software Foundation. For the terms of this 
+ license, see license/gpl-3.txt or <http://www.gnu.org/licenses/>.
+
+  You are free to use this library under the terms of the GNU General
+ Public License, but WITHOUT ANY WARRANTY; without even the implied 
+ warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ See the GNU General Public License for more details.
+
+  Alternatively, you can use this library under any other
+ OSS licences ( like BSD ) or your own commercial license,
+ as set out in LICENSE.txt. */
+
+
+#ifndef __CORAL_CRYPTO_SHA256_H
+#	define __CORAL_CRYPTO_SHA256_H
+
+#include <coral/def.h>
+
+#	define SHA256_HASH_SET_COUNT 8  /* prime number hash set */
+#	define SHA256_LENGTH         32 /* 8 * 4 */
+#	define SHA256_CHUNK          64 /* 512 bits */
+
+__CBEGIN__
+
+
+
+__CEND__
+
+#endif /* __CORAL_CRYPTO_SHA256_H */
